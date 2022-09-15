4e014d6f8f28350e02dbcede796d930577f420ac
Ardavan-Shamroshan <ardavan2000.ashr@gmail.com>
2022-09-15 21:38:50 +0430

    google recaptcha added

--- resources/views/livewire/home/layouts/head-tag.blade.php
+++ resources/views/livewire/home/layouts/head-tag.blade.php
@@ -1,6 +1,10 @@
     <meta charset="utf-8">
     <title>{{ $setting->title }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+     <!-- IMPORTANT!!! remember CSRF token -->
+     <meta name="csrf-token" content="{{ csrf_token() }}">
+
     <!-- Template home-assets/css Files -->
     <link type="text/css" media="all" href="{{ asset('home-assets/css/bootstrap.min.css') }}" rel="stylesheet">
     <link type="text/css" media="all" href="{{ asset('home-assets/css/jquery.animatedheadline.css') }}" rel="stylesheet">
@@ -13,4 +17,5 @@
     <!-- Template JS Files -->
     <script src="{{ asset('home-assets/js/modernizr.js') }}"></script>
 
+    {!! htmlScriptTagJsApi() !!}
     @livewireStyles
